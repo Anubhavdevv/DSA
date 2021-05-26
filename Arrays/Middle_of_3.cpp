65a98ff180c230c47fe0ba6bc46c7968732da598
Anubhav <72249059+anubhav201241@users.noreply.github.com>
2021-05-26 22:59:36 +0530

    Create Middle_of_3.cpp
    Given three distinct numbers A, B and C. Find the number with a value in the middle

---
+++ Arrays/Middle_of_3.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution{
+  public:
+    int middle(int A, int B, int C){
+        if(B<max(A,C) && B>min(A,C)){
+             return B;
+        }
+        else if(A<max(B,C) && A>min(B,C)){
+              return A;
+        }
+        else{
+            return C;
+        }
+            
+    }
+};
+
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int A,B,C;
+        cin>>A>>B>>C;
+        Solution ob;
+        cout<<ob.middle(A,B,C) <<"\n";
+    }
+    return 0;
+}  
